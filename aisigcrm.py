6dc0bc5e2772b32ed0bab013563457c5e7a655da
johntellojohn <john-hy@hotmail.fr>
2025-02-18 16:39:43 -0500

    cambios

--- aisigcrm.py
+++ aisigcrm.py
@@ -542,9 +542,10 @@ def chatbot():
             print("\nPROMPT PARA GPT:\n", prompt)
 
             # Llamar a la API de OpenAI con el formato actualizado
-            openai.api_key = os.getenv('OPENAI_API_KEY')
+            client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
             print("TOKEN:", os.getenv('OPENAI_API_KEY'))
             try:
+                print("TOKEN1:", os.getenv('OPENAI_API_KEY'))
                 respuesta_gpt = client.chat.completions.create(
                     model="gpt-3.5-turbo",  # Usar un modelo válido
                     messages=[
